ac02fd9431b0e528df6279e4425d54537a803ff0
Carlo D'Ambrosio <carlo.dambrosio@lovetoknow.com>
2020-05-21 10:48:25 +0200

    Switch to remote state

---
+++ terraform.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "yd-sample-terraform-state"
+}
+
+terraform {
+    backend "s3" {
+        region  = "us-east-1"
+        bucket  = "yd-sample-terraform-state"
+        key     = "terraform.tfstate"
+    }
+}
